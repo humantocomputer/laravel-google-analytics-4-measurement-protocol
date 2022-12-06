d1b4213a872652ae405453149d85f098e234ca9f
Yohann <yohann@humantocomputer.com>
2022-12-06 18:22:47 +0100

    fix route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Freshbitsweb\LaravelGoogleAnalytics4MeasurementProtocol\Http\Controllers\StoreGoogleAnalyticsClientIdController;
+use Freshbitsweb\LaravelGoogleAnalytics4MeasurementProtocol\Http\Controllers\StoreGoogleAnalyticsSessionIdController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('store-google-analytics-client-id', StoreGoogleAnalyticsClientIdController::class)->middleware('web');
